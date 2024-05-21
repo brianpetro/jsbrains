5c45b293211f45eead321868a08b9a70d647f901
WFH Brian <brian@plexm.com>
2024-05-21 08:36:07 -0400

    worker

--- smart-ranker-model/adapters.js
+++ smart-ranker-model/adapters.js
@@ -2,3 +2,6 @@ const { CohereAdapter } = require('./adapters/cohere');
 exports.Cohere = CohereAdapter;
 const { TransformersAdapter } = require('./adapters/transformers');
 exports.Transformers = TransformersAdapter;
+const { WorkerAdapter } = require('./adapters/worker');
+exports.Worker = WorkerAdapter;
+
--- smart-ranker-model/adapters/worker.js
+++ smart-ranker-model/adapters/worker.js
@@ -3,18 +3,26 @@ const { Adapter } = require("./adapter");
 class WorkerAdapter extends Adapter {
   async init() {
     if (!this.worker) {
-      this.worker = create_worker('./worker_script.js');
+      this.worker = create_worker();
       this.worker.postMessage({
         type: 'init',
         config: this.worker_config
       });
 
       await new Promise((resolve) => {
-        this.worker.on('message', (data) => {
-          if (data.status === 'ready') {
-            resolve();
-          }
-        });
+        if(typeof this.worker.on === 'function') {
+          this.worker.on('message', (data) => {
+            if (data.status === 'ready') {
+              resolve();
+            }
+          });
+        } else {
+          this.worker.onmessage = (event) => {
+            if (event.data.status === 'ready') {
+              resolve();
+            }
+          };
+        }
       });
     }
   }
@@ -27,11 +35,19 @@ class WorkerAdapter extends Adapter {
     });
 
     const ranked_documents = await new Promise((resolve) => {
-      this.worker.on('message', (data) => {
-        if (data.type === 'ranked_documents') {
-          resolve(data.ranked_documents);
-        }
-      });
+      if(typeof this.worker.on === 'function') {
+        this.worker.on('message', (data) => {
+          if (data.type === 'ranked_documents') {
+            resolve(data.ranked_documents);
+          }
+        });
+      } else {
+        this.worker.onmessage = (event) => {
+          if (event.data.type === 'ranked_documents') {
+            resolve(event.data.ranked_documents);
+          }
+        };
+      }
     });
     return ranked_documents;
   }
@@ -49,10 +65,132 @@ if (is_node) {
   Worker = window.Worker;
 }
 
-function create_worker(worker_script) {
+function create_worker() {
   if (is_node) {
-    return create_node_worker(worker_script);
+    return create_node_worker('./worker_script.js');
   } else {
+    const worker_script = `
+class Adapter {
+  /**
+   * Constructs an instance of Adapter.
+   * @param {object} main - The main context object which should contain a configuration object.
+   */
+  constructor(main) {
+    /**
+     * The main context object from which configuration is derived.
+     * @type {object}
+     */
+    this.main = main;
+
+    /**
+     * Copies properties from the main object's config property to this instance.
+     */
+    Object.assign(this, main.config); // Copy config to this
+  }
+}
+class TransformersAdapter extends Adapter {
+    async init() {
+      console.log('TransformersAdapter initializing');
+      const { env, AutoTokenizer, AutoModelForSequenceClassification } = await import('https://cdn.jsdelivr.net/npm/@xenova/transformers@latest');
+      console.log('Transformers loaded');
+      env.allowLocalModels = false;
+      const model_id = this.model_key;
+      if(this.quantized) {
+        console.log('Quantized model loading');
+      }
+      this.tokenizer = await AutoTokenizer.from_pretrained(model_id);
+      console.log('Tokenizer loaded');
+      this.model = await AutoModelForSequenceClassification.from_pretrained(model_id, { quantized: this.quantized || false });
+      console.log('Model loaded');
+      console.log('TransformersAdapter initialized');
+    }
+
+    async rank(query, documents, options = {}) {
+        const { top_k = undefined, return_documents = false } = options;
+        const inputs = this.tokenizer(
+            new Array(documents.length).fill(query),
+            { text_pair: documents, padding: true, truncation: true }
+        );
+        const { logits } = await this.model(inputs);
+        return logits.sigmoid().tolist()
+            .map(([score], i) => ({
+                corpus_id: i,
+                score,
+                ...(return_documents ? { text: documents[i] } : {})
+            })).sort((a, b) => b.score - a.score).slice(0, top_k);
+    }
+}
+class SmartRankerModel {
+  constructor(env, config) {
+    this.env = env;
+    // if(config.model_key) this.config = {...models[config.model_key], ...config};
+    // else this.config = { ...config };
+    this.config = { ...config };
+    console.log(this.config);
+    this.adapter = new TransformersAdapter(this);
+  }
+  async rank(query, documents) {
+    return this.adapter.rank(query, documents);
+  }
+  async init() {
+    if(typeof this.adapter.init === 'function') await this.adapter.init();
+  }
+}
+
+let model;
+async function handle_incoming_data(data) {
+  console.log('handle_incoming_data', data);
+  try {
+    switch (data.type) {
+      case 'init':
+        console.log('init', data.config);
+        model = new SmartRankerModel({}, data.config);
+        await model.init();
+        return { status: 'ready' };
+      case 'rank':
+        return { type: 'ranked_documents', ranked_documents: await model.rank(data.query, data.documents) };
+    }
+  } catch (error) {
+    console.error('Error in handle_incoming_data', error);
+    return { status: 'error', error: error.message };
+  }
+}
+
+if (typeof self !== 'undefined') {
+  // // service worker script
+  // self.addEventListener("fetch", (event) => {
+  //   event.respondWith(
+  //     (async () => {
+  //       let request, cache, response;
+  //       request = event.request;
+  //       cache = await caches.open("transformers-cache");
+  //       response = await cache.match(request);
+  //       if (response) return response;
+
+  //       response = await fetch(request);
+  //       if (response.status === 200 && !request.url.match(/onnx$|json$/)) {
+  //         // cache files other than onnx and json (cached by transformers.js)
+  //         cache.put(request, response.clone());
+  //       }
+  //       return response;
+  //     })(),
+  //   );
+  // });
+  // Browser environment
+  self.onmessage = async function(event) {
+    const data = event.data;
+    const result = await handle_incoming_data(data);
+    self.postMessage(result);
+  };
+} else if (typeof require !== 'undefined') {
+  // Node.js environment
+  const { parentPort } = require('worker_threads');
+
+  parentPort.on('message', async (data) => {
+    const result = await handle_incoming_data(data);
+    parentPort.postMessage(result);
+  });
+}`;
     return create_browser_worker(worker_script);
   }
 }
@@ -72,9 +210,10 @@ function create_node_worker(worker_script) {
   }
 }
 
-function create_browser_worker(worker_script) {
+function create_browser_worker(worker_script_string) {
   try {
-    const worker = new Worker(worker_script);
+    const blob = new Blob([worker_script_string], { type: 'application/javascript' });
+    const worker = new Worker(URL.createObjectURL(blob));
 
     worker.onerror = (error) => {
       console.error('Worker error:', error);
