aceb203425382a659d4645a3c4f2b5cb2bd5a5b3
Brian Joseph Petro <brian@smartconnections.app>
2024-08-02 17:54:04 -0400

    moved to zArchive

--- smart-collections/ObsAJSON.js
+++
@@ -1,7 +0,0 @@
-// DEPRECATED: file for backward compatibility with Smart Collections v2.1
-class ObsAJSON {
-  constructor(env) {
-    this.env = env;
-  }
-}
-exports.ObsAJSON = ObsAJSON;
--- smart-collections/ObsMultiAJSON.js
+++
@@ -1,9 +0,0 @@
-// DEPRECATED: file for backward compatibility with Smart Collections v2.1
-
-class ObsMultiAJSON {
-  constructor(env) {
-    this.env = env;
-  }
-}
-
-exports.ObsMultiAJSON = ObsMultiAJSON;
