e4ed85bde0e10704dcfa95042f9ed954f3aaed80
Brian Joseph Petro <brian@smartconnections.app>
2024-08-27 20:11:32 -0400

    fix saving vectors after embed

--- smart-entities/adapters/_adapter.js
+++ smart-entities/adapters/_adapter.js
@@ -3,19 +3,19 @@ export class EntityAdapter {
     this.smart_entity = smart_entity;
   }
   get data() { return this.smart_entity.data; }
-  get embed_model() { return this.smart_entity.embed_model; }
+  get embed_model_key() { return this.smart_entity.embed_model_key; }
 
   get vec() {
-    return this.data?.embeddings?.[this.embed_model]?.vec;
+    return this.data?.embeddings?.[this.embed_model_key]?.vec;
   }
 
   set vec(vec) {
     if (!this.data.embeddings) {
       this.data.embeddings = {};
     }
-    if (!this.data.embeddings[this.embed_model]) {
-      this.data.embeddings[this.embed_model] = {};
+    if (!this.data.embeddings[this.embed_model_key]) {
+      this.data.embeddings[this.embed_model_key] = {};
     }
-    this.data.embeddings[this.embed_model].vec = vec;
+    this.data.embeddings[this.embed_model_key].vec = vec;
   }
 }
