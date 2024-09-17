1bb89676e8a8773134229d2c41417c1368dc4767
WFH Brian <brian@plexm.com>
2024-09-16 21:34:02 -0400

    update to use smart_view binding

--- smart-chats-v2/components/settings.js
+++ smart-chats-v2/components/settings.js
@@ -1,10 +1,4 @@
-import { create_document_fragment } from "./_component.js";
 export async function template(collection, opts = {}) {
-  const smart_settings = new collection.env.smart_chat_plugin.smart_settings_class(collection.env, {
-    smart_setting_class: collection.env.smart_chat_plugin.smart_setting_class, // These setting classes should be moved to env.opts
-    rel_instance: collection,
-    re_render: collection.render_settings.bind(collection),
-  });
   const html = Object.entries(collection.settings_config).map(([setting_name, setting_config]) => {
     const attributes = Object.entries(setting_config)
       .map(([attr, value]) => `data-${attr.replace(/_/g, '-')}="${value}"`)
@@ -12,8 +6,8 @@ export async function template(collection, opts = {}) {
       ;
     return `<div class="setting-component"\ndata-setting="${setting_name}"\n${attributes}\n></div>`;
   }).join('\n');
-  const frag = create_document_fragment(html);
-  await smart_settings.render_components(frag);
+  const frag = this.create_doc_fragment(html);
+  await this.render_settings_components(frag);
   return frag;
 }
 
