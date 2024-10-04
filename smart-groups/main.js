0dae70cdc74a0ee6e2e606c6bde322b80deb2bdb
Brian Joseph Petro <brian@smartconnections.app>
2024-10-04 15:14:06 -0400

    clean-up and init smart-groups

---
+++ smart-groups/main.js
@@ -0,0 +1,8 @@
+import { SmartDirectory } from "./smart_directory.js";
+import { SmartDirectories } from "./smart_directories.js";
+
+export {
+  SmartDirectory,
+  SmartDirectories,
+};
+
--- smart-sources-v1/utils/increase_heading_depth.js
+++
@@ -1,5 +0,0 @@
-
-
-export function increase_heading_depth(content, depth) {
-  return content.replace(/^(#+)/gm, match => '#'.repeat(match.length + depth));
-}
