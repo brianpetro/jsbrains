f27d15ec69e58ba7c9154c3e5de9ce8bcab3285e
Brian Joseph Petro <brian@smartconnections.app>
2024-10-23 12:48:23 -0400

    update anthropic models re: https://github.com/brianpetro/obsidian-smart-connections/issues/848

--- smart-chat-model-v1/models/anthropic.js
+++ smart-chat-model-v1/models/anthropic.js
@@ -2,6 +2,14 @@
 async function fetch_anthropic_models() {
   return [
     {
+      key: "claude-3-5-sonnet-latest",
+      "model_name": "claude-3.5-sonnet-latest",
+      "description": "Anthropic's Claude Sonnet (Latest)",
+      "max_input_tokens": 200000,
+      "max_output_tokens": 4000,
+      "multimodal": true
+    },
+    {
       "key": "claude-3-opus-20240229",
       "model_name": "claude-3-opus-20240229",
       "description": "Anthropic's Claude Opus",
@@ -10,20 +18,28 @@ async function fetch_anthropic_models() {
       "multimodal": true
     },
     {
-      key: "claude-3-5-sonnet-20240620",
-      "model_name": "claude-3.5-sonnet-20240620",
-      "description": "Anthropic's Claude Sonnet",
+      key: "claude-3-haiku-20240307",
+      "model_name": "claude-3-haiku-20240307",
+      "description": "Anthropic's Claude Haiku (2024-03-07)",
       "max_input_tokens": 200000,
       "max_output_tokens": 4000,
       "multimodal": true
     },
     {
-      key: "claude-3-haiku-20240307",
-      "model_name": "claude-3-haiku-20240307",
-      "description": "Anthropic's Claude Haiku",
+      key: "claude-3-5-sonnet-20241022",
+      "model_name": "claude-3.5-sonnet-20241022",
+      "description": "Anthropic's Claude Sonnet (2024-10-22)",
       "max_input_tokens": 200000,
-        "max_output_tokens": 4000,
-        "multimodal": true
+      "max_output_tokens": 4000,
+      "multimodal": true
+    },
+    {
+      key: "claude-3-5-sonnet-20240620",
+      "model_name": "claude-3.5-sonnet-20240620",
+      "description": "Anthropic's Claude Sonnet (2024-06-20)",
+      "max_input_tokens": 200000,
+      "max_output_tokens": 4000,
+      "multimodal": true
     },
     {
       key: "claude-3-sonnet-20240229",
--- smart-chat-model/adapters/anthropic.js
+++ smart-chat-model/adapters/anthropic.js
@@ -7,6 +7,14 @@ export class SmartChatModelAnthropicAdapter extends SmartChatModelApiAdapter {
   async get_models() {
     return [
       {
+        key: "claude-3-5-sonnet-latest",
+        "model_name": "claude-3.5-sonnet-latest",
+        "description": "Anthropic's Claude Sonnet (Latest)",
+        "max_input_tokens": 200000,
+        "max_output_tokens": 4000,
+        "multimodal": true
+      },
+      {
         "key": "claude-3-opus-20240229",
         "model_name": "claude-3-opus-20240229",
         "description": "Anthropic's Claude Opus",
@@ -15,20 +23,28 @@ export class SmartChatModelAnthropicAdapter extends SmartChatModelApiAdapter {
         "multimodal": true
       },
       {
-        key: "claude-3-5-sonnet-20240620",
-        "model_name": "claude-3.5-sonnet-20240620",
-        "description": "Anthropic's Claude Sonnet",
+        key: "claude-3-haiku-20240307",
+        "model_name": "claude-3-haiku-20240307",
+        "description": "Anthropic's Claude Haiku (2024-03-07)",
         "max_input_tokens": 200000,
         "max_output_tokens": 4000,
         "multimodal": true
       },
       {
-        key: "claude-3-haiku-20240307",
-        "model_name": "claude-3-haiku-20240307",
-        "description": "Anthropic's Claude Haiku",
+        key: "claude-3-5-sonnet-20241022",
+        "model_name": "claude-3.5-sonnet-20241022",
+        "description": "Anthropic's Claude Sonnet (2024-10-22)",
         "max_input_tokens": 200000,
-          "max_output_tokens": 4000,
-          "multimodal": true
+        "max_output_tokens": 4000,
+        "multimodal": true
+      },
+      {
+        key: "claude-3-5-sonnet-20240620",
+        "model_name": "claude-3.5-sonnet-20240620",
+        "description": "Anthropic's Claude Sonnet (2024-06-20)",
+        "max_input_tokens": 200000,
+        "max_output_tokens": 4000,
+        "multimodal": true
       },
       {
         key: "claude-3-sonnet-20240229",
