46361c67aa9a75d85566e368557768807b63003a
Brian Joseph Petro <brian@smartconnections.app>
2024-12-04 19:52:49 -0500

    Refactor rendering logic in SmartEntities and SmartSources
    - Updated the `render` function in `entity.js` to handle rendering based on entity type and embed links more effectively.
    - Introduced `process_for_rendering` to sanitize content by preventing unwanted dataview rendering and link embedding.
    - Enhanced `should_render_embed` logic to streamline embed checks for both entities and sources.
    - Refactored `render` in `source.js` to utilize the new rendering functions from `entity.js`, improving code reuse and clarity.
    - Removed outdated `get_markdown` and `post_process` functions from `source.js`, simplifying the codebase.
    
    Prevents displaying embedded links in Smart Connections view

--- smart-entities/components/entity.js
+++ smart-entities/components/entity.js
@@ -1,25 +1,28 @@
-export async function render(scope, opts = {}) {
-  const markdown = await get_markdown(scope);
-  const frag = await this.render_markdown(markdown, scope);
-  return await post_process.call(this, scope, frag, opts);
+export async function render(entity, opts = {}) {
+  const markdown = should_render_embed(entity)
+    ? entity.embed_link
+    : process_for_rendering(await entity.get_content())
+  ;
+  let frag;
+  if(entity.env.settings.smart_view_filter.render_markdown) frag = await this.render_markdown(markdown, entity);
+  else frag = this.create_doc_fragment(markdown);
+  return await post_process.call(this, entity, frag, opts);
 }
 
-async function get_markdown(scope) {
-  return should_render_embed(scope)
-    ? scope.embed_link
-    : (await scope.get_content())?.replace(/```dataview/g, '```\\dataview')
-  ;
+export function process_for_rendering(content) {
+  // prevent dataview rendering
+  if(content.includes('```dataview')) content = content.replace(/```dataview/g, '```\\dataview');
+  // prevent link embedding
+  if(content.includes('![[')) content = content.replace(/\!\[\[/g, '! [[');
+  return content;
 }
 
 export async function post_process(scope, frag, opts = {}) {
   return frag;
 }    
 
-function should_render_embed(entity) {
+export function should_render_embed(entity) {
   if (!entity) return false;
-  if (entity.is_canvas) return true;
-  if (entity.is_excalidraw) return true;
-  if (entity.source?.is_canvas) return true;
-  if (entity.source?.is_excalidraw) return true;
+  if (entity.is_canvas || entity.is_excalidraw) return true;
   return false;
 }
--- smart-sources/components/source.js
+++ smart-sources/components/source.js
@@ -1,26 +1,22 @@
-export async function render(scope, opts = {}) {
-  const markdown = await get_markdown(scope);
-  let frag;
-  if(scope.env.settings.smart_view_filter.render_markdown) frag = await this.render_markdown(markdown, scope);
-  else frag = this.create_doc_fragment(`<span>${markdown}</span>`);
-  return await post_process.call(this, scope, frag, opts);
-}
-
-async function get_markdown(scope) {
-  return should_render_embed(scope)
-    ? scope.embed_link
-    : (await scope.get_content())?.replace(/```dataview/g, '```\\dataview')
+import {
+  process_for_rendering,
+  should_render_embed,
+  post_process,
+} from "smart-entities/components/entity.js";
+export async function render(source, opts = {}) {
+  const markdown = should_render_embed_source(source)
+    ? source.embed_link
+    : process_for_rendering(await source.get_content())
   ;
+  let frag;
+  if(source.env.settings.smart_view_filter.render_markdown) frag = await this.render_markdown(markdown, source);
+  else frag = this.create_doc_fragment(`<span>${markdown}</span>`);
+  return await post_process.call(this, source, frag, opts);
 }
 
-export async function post_process(scope, frag, opts = {}) {
-  return frag;
-}    
-
-function should_render_embed(entity) {
-  if (!entity) return false;
-  if (entity.is_canvas || entity?.source?.is_canvas) return true;
-  if (entity.is_excalidraw || entity?.source?.is_excalidraw) return true;
-  if (entity.file_type !== 'md') return true;
+function should_render_embed_source(source) {
+  if(should_render_embed(source)) return true;
+  if (entity.source?.is_canvas || entity.source?.is_excalidraw) return true;
+  if (source.file_type !== 'md') return true;
   return false;
 }
