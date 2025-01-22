07476eee8c0b03def3f8334c238901cca0e398ba
Brian Joseph Petro <brian@smartconnections.app>
2025-01-22 10:19:18 -0500

    Update clone method in ClusterGroup to handle optional chaining for add_clusters and remove_clusters
    - Modified the `clone` method to use optional chaining when mapping `add_clusters` and `remove_clusters`, ensuring that the code handles cases where these options may be undefined. This change improves the robustness of the cluster management functionality.

--- smart-cluster-groups/cluster_group.js
+++ smart-cluster-groups/cluster_group.js
@@ -101,8 +101,8 @@ export class ClusterGroup extends CollectionItem {
    * @returns {Promise<ClusterGroup>}
    */
   async clone(opts = {}) {
-    opts.add_clusters = opts.add_clusters.map(c => typeof c === 'string' ? c : c.key);
-    opts.remove_clusters = opts.remove_clusters.map(c => typeof c === 'string' ? c : c.key);
+    opts.add_clusters = opts.add_clusters?.map(c => typeof c === 'string' ? c : c.key);
+    opts.remove_clusters = opts.remove_clusters?.map(c => typeof c === 'string' ? c : c.key);
     const new_clusters = (opts.add_clusters || []).reduce((acc, key) => {
       acc[key] = { filters: {} };
       return acc;
