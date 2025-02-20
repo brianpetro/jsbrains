1fe52dc014d5ffb42af5617023c4d32496aaa2db
WFH Brian <brian@plexm.com>
2025-02-20 13:08:19 -0500

    - Added static `create()` method for SmartEnv initialization - Implemented file event listeners for create, rename, and modify events - Added automatic file path initialization and inclusion for Obsidian vault files - Introduced file modification import queue with debounce timeout - Imported TFile type from Obsidian for file type checking

--- smart-environment/obsidian.js
+++ smart-environment/obsidian.js
@@ -6,7 +6,7 @@ import { SmartViewObsidianAdapter } from 'smart-view/adapters/obsidian.js';
 // import { SmartNotices } from "../../sc-obsidian/src/smart_notices.js";
 // import { Notice } from "obsidian";
 import { merge_options } from './utils/merge_options.js';
-
+import { TFile } from 'obsidian';
 
 const OBSIDIAN_DEFAULTS = {
   env_path: '',
@@ -27,8 +27,41 @@ const OBSIDIAN_DEFAULTS = {
 };
 
 export class SmartEnv extends BaseSmartEnv {
-  constructor(opts = {}) {
-    opts = merge_options(opts, OBSIDIAN_DEFAULTS);
-    super(opts);
+  static async create(plugin, main_env_opts = {}) {
+    const opts = merge_options(main_env_opts, OBSIDIAN_DEFAULTS);
+    return await super.create(plugin, opts);
+  }
+  async init(plugin, main_env_opts = {}) {
+    plugin.registerEvent(
+      plugin.app.vault.on('create', (file) => {
+        if(file instanceof TFile){
+          this.smart_sources?.init_file_path(file.path);
+          this.smart_sources?.fs.include_file(file.path);
+        }
+      })
+    );
+    plugin.registerEvent(
+      plugin.app.vault.on('rename', (file) => {
+        if(file instanceof TFile){
+          this.smart_sources?.init_file_path(file.path);
+          this.smart_sources?.fs.include_file(file.path);
+        }
+      })
+    );
+    plugin.registerEvent(
+      plugin.app.vault.on('modify', (file) => {
+        if(file instanceof TFile){
+          const source = this.smart_sources?.get(file.path);
+          if(source){
+            source.queue_import();
+            if(this.sources_import_timeout) clearTimeout(this.sources_import_timeout);
+            this.sources_import_timeout = setTimeout(() => {
+              source.import();
+            }, 3000);
+          }
+        }
+      })
+    );
+    return await super.init(plugin, main_env_opts);
   }
 }
