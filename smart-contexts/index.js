7c30752a852ad853b827230a2aa8baf584593219
Brian Joseph Petro <brian@smartconnections.app>
2025-02-26 17:59:55 -0500

    - Add default configuration for SmartContexts - Import AjsonSingleFileCollectionDataAdapter as default data adapter - Export smart_contexts with default configuration

--- smart-contexts/index.js
+++ smart-contexts/index.js
@@ -1,4 +1,14 @@
 import { SmartContexts } from './smart_contexts.js';
 import { SmartContext } from './smart_context.js';
+import { AjsonSingleFileCollectionDataAdapter } from 'smart-collections/adapters/ajson_single_file.js';
 
-export { SmartContexts, SmartContext };
+export const smart_contexts_default_config = {
+  class: SmartContexts,
+  data_adapter: AjsonSingleFileCollectionDataAdapter,
+}
+
+export {
+  SmartContexts,
+  SmartContext,
+  smart_contexts_default_config as smart_contexts
+};
