f0a645854c07fb0884d758cb2019bd32c3e7e3a6
Brian Joseph Petro <brian@smartconnections.app>
2025-08-18 15:51:56 -0400

    fix: bump SmartEnv version to 2.139266 for consistency

--- smart-environment/smart_env.js
+++ smart-environment/smart_env.js
@@ -43,7 +43,7 @@ export class SmartEnv {
    * If a newer version is loaded into a runtime that already has an older environment,
    * an automatic reload of all existing mains will occur.
    */
-  static version = 2.139265;
+  static version = 2.139266;
   scope_name = 'smart_env';
   static global_ref = ROOT_SCOPE;
   global_ref = this.constructor.global_ref;
